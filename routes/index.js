15c5ab801dcf1225eaa26948ec6e382f53676a1b
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2018-09-18 00:27:11 -0500

    :-1:
    :-1:

--- routes/index.js
+++ routes/index.js
@@ -759,7 +759,7 @@ app.delete('/schedule/remove', (req, res, next) => {
 
 
 app.get('/*', function(req, res) {
-    res.sendFile('../public/index.html');
+    res.sendFile(__dirname +'../client/public/index.html');
 })
 
 module.exports = app;
