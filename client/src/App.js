93381fd182f3fe8eec56dbe5abdac374723f1ed4
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2018-09-17 23:34:11 -0500

    imports missing
    adding imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,7 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
 class App extends Component {
   render() {
     const App = () => (
