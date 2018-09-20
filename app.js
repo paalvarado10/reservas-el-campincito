7d908d4bf215262a50473902a21396738d651d16
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2018-09-19 23:18:33 -0500

    CLIENT NOT FRONT

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.static(path.join(__dirname,'front/build')));
 app.use('/', index);
 app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, 'front/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
 // catch 404 and forward to error handler
