a0ba609f2cf736809142ae6e83f24f629f539744
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2018-09-19 23:09:46 -0500

    s

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client')));
-
+app.use(express.static(path.join(__dirname,'front/build')));
 app.use('/', index);
 
 // catch 404 and forward to error handler
